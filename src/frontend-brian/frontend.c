b57d757112f8bac0025096842c6cea4a7987e60e
Brian-ED <brianellingsgaard9@gmail.com>
2024-11-18 23:04:55 +0100

    Added great background

--- src/frontend-brian/frontend.c
+++ src/frontend-brian/frontend.c
@@ -18,8 +18,9 @@ void Start() {
   InitWindow(screenWidth, screenHeight, "raylib [core] example - window flags");
 
   Texture tex = LoadTexture("../../content/workout-images.png");
-  Vector2 texPosition = {.x=-400, .y=-300};
+  Vector2 texPosition = {.x=0, .y=0};
   Rectangle texSource = {.x=0, .y=0, .width=936, .height=1000};
+  Color titleColor = BLACK;
 
   // run at 60 frames-per-second
   SetTargetFPS(60);
@@ -39,8 +40,8 @@ void Start() {
     }
 
     BeginDrawing();
-      texPosition.x += 2;
-      texPosition.y += 1;
+      texPosition.x = (float)((2 + (int)texPosition.x) % (int)texSource.width);
+      texPosition.y = (float)((1 + (int)texPosition.y) % (int)texSource.height);
       ClearBackground(BLACK);
 
       if (window_mode == WINDOW_MODE_MAIN_MENU) {
@@ -48,15 +49,20 @@ void Start() {
           // Get color lerp interpolation between two colors, factor [0.0f..1.0f]
           DrawRectangle(0, 0, screenWidth, screenHeight, Fade(RAYWHITE, (float)frames_since_transition/60.0f));
         } else if (frames_since_transition<60*2) {
-          Color blackFade = Fade(BLACK, (float)(frames_since_transition-60)/60.0f);
+          Color titleFade = Fade(titleColor, (float)(frames_since_transition-60)/60.0f);
           Color whiteFade = Fade(WHITE, (float)(frames_since_transition-60)/60.0f);
-          DrawTextureRec(tex, texSource, texPosition, whiteFade);            // Draw a part of a texture defined by a rectangle
-          DrawText(fitnessTitle, (screenWidth-LengthOfFitnessTitle)/2 , screenHeight/2 - 50, fontSize, blackFade);
+          DrawTextureRec(tex, texSource, (Vector2){texPosition.x                  , texPosition.y                   }, whiteFade);            // Draw a part of a texture defined by a rectangle
+          DrawTextureRec(tex, texSource, (Vector2){texPosition.x - texSource.width, texPosition.y                   }, whiteFade);            // Draw a part of a texture defined by a rectangle
+          DrawTextureRec(tex, texSource, (Vector2){texPosition.x                  , texPosition.y - texSource.height}, whiteFade);            // Draw a part of a texture defined by a rectangle
+          DrawTextureRec(tex, texSource, (Vector2){texPosition.x - texSource.width, texPosition.y - texSource.height}, whiteFade);            // Draw a part of a texture defined by a rectangle
+          DrawText(fitnessTitle, (screenWidth-LengthOfFitnessTitle)/2 , screenHeight/2 - 50, fontSize, titleFade);
           ClearBackground(RAYWHITE);
         } else {
-          DrawTextureRec(tex, texSource, texPosition, WHITE);            // Draw a part of a texture defined by a rectangle
+          DrawTextureRec(tex, texSource, (Vector2){texPosition.x                  , texPosition.y                   }, WHITE);            // Draw a part of a texture defined by a rectangle
+          DrawTextureRec(tex, texSource, (Vector2){texPosition.x - texSource.width, texPosition.y                   }, WHITE);            // Draw a part of a texture defined by a rectangle
+          DrawTextureRec(tex, texSource, (Vector2){texPosition.x                  , texPosition.y - texSource.height}, WHITE);            // Draw a part of a texture defined by a rectangle
+          DrawTextureRec(tex, texSource, (Vector2){texPosition.x - texSource.width, texPosition.y - texSource.height}, WHITE);            // Draw a part of a texture defined by a rectangle
           DrawText(fitnessTitle, (screenWidth-LengthOfFitnessTitle)/2, screenHeight/2 - 50, fontSize, BLACK);
-          ClearBackground(RAYWHITE);
         }
         DrawFPS(10, 10);
       }
