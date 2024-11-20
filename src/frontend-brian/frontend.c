1089a4228964188cf4a426715522f8ac8b27acd1
Brian-ED <brianellingsgaard9@gmail.com>
2024-11-20 10:13:18 +0100

    Added raygui and made UI scale well. also button!

--- src/frontend-brian/frontend.c
+++ src/frontend-brian/frontend.c
@@ -1,4 +1,11 @@
 #include <raylib.h>
+#include <raymath.h>
+#include <stdio.h>
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.c"
+
+#define BACKGROUND_SPEED 0.5
 
 typedef enum {
   WINDOW_MODE_MAIN_MENU,
@@ -6,69 +13,135 @@ typedef enum {
 
 WindowMode window_mode = WINDOW_MODE_MAIN_MENU;
 
+Vector2 GetWindowSize() {
+    return (Vector2){GetScreenWidth(), GetScreenHeight()};
+};
+
+Vector2 Vector2Mod(Vector2 divided, Vector2 divider) {
+    int xd = (int)divider.x;
+    int yd = (int)divider.y;
+    return (Vector2){
+        (float)( ((int)divided.x % xd + xd) % xd),
+        (float)( ((int)divided.y % yd + yd) % yd),
+    };
+}
+
+Rectangle Vector2ToRectangle(Vector2 v1, Vector2 size) {
+    return (Rectangle){.x = v1.x, .y = v1.y, .width = size.x, .height = size.y};
+}
+
+float Min(Vector2 v) {
+    return v.x>v.y ?v.x :v.y;
+}
+float Max(Vector2 v) {
+    return v.x>v.y ?v.y :v.x;
+}
+
+// Draw text (using default font)
+void DrawTextV(const char *text, Vector2 pos, int fontSize, Color color) {
+    DrawText(text, pos.x, pos.y, fontSize, color);
+}
+
+void DrawBackgroundTexture(Texture tex, Rectangle texSource, Vector2 texOffset, float fade) {
+    Color whiteFade = Fade(WHITE, fade);
+    Vector2 texSize = {texSource.width, texSource.height};
+    Vector2 backgroundSize = GetWindowSize();
+
+    for (float i=texOffset.x; i<backgroundSize.x; i+=texSource.width) {
+        for (float j=texOffset.y; j<backgroundSize.y; j+=texSource.height) {
+            DrawTextureRec(tex, texSource, (Vector2){i, j}, whiteFade); // Draw a part of a texture defined by a rectangle
+        }
+    }
+}
+
 void Start() {
-  SetTraceLogLevel(LOG_WARNING); // Make it so that only important warnings are printed out
+    SetTraceLogLevel(LOG_WARNING); // Make it so that only important warnings are printed out
 
-  const int screenWidth = 800;
-  const int screenHeight = 450;
-  const int fontSize = 40;
+    Vector2 windowSize = {800.0, 450.0};
 
-  int frames_since_transition = 0;
+    float seconds_since_transition = 0;
 
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - window flags");
+    InitWindow((int)windowSize.x, (int)windowSize.y, "Fitness");
+    SetExitKey(KEY_NULL);
 
-  Texture tex = LoadTexture("../../content/workout-images.png");
-  Vector2 texPosition = {.x=0, .y=0};
-  Rectangle texSource = {.x=0, .y=0, .width=936, .height=1000};
-  Color titleColor = BLACK;
+    bool buttonOn = false;
 
-  // run at 60 frames-per-second
-  SetTargetFPS(60);
+    Texture tex = LoadTexture("../../content/workout-images.png");
+    Vector2 texPosition = {.x=0, .y=0};
+    Vector2 texSize = {.x=936, .y=1000};
+    Rectangle texSource = {.x=0, .y=0, .width=texSize.x, .height=texSize.y};
+    Color titleColor = BLACK;
 
-  char *fitnessTitle = "Fitness";
-  int LengthOfFitnessTitle = MeasureText(fitnessTitle, fontSize);
+    // run at 60 frames-per-second
+    int fps = 60;
+    SetTargetFPS(fps);
 
-  while (!WindowShouldClose()) {
+    while (!WindowShouldClose()) {
+        windowSize = GetWindowSize();
+        Vector2 screenMiddle = Vector2Scale(windowSize, 0.5);
+        int fontSize = Min(windowSize)/10;
 
-    WindowMode last_window_mode = window_mode;
-    int window_changed = (last_window_mode != window_mode);
+        WindowMode last_window_mode = window_mode;
+        int window_changed = (last_window_mode != window_mode);
 
-    frames_since_transition += 1;
+        // x frame = 1 second
+        // x = second/frame
+        // fps = frame/second
+        // x = 1/fps
+        seconds_since_transition += 1.0/(float)fps;
 
-    if (window_changed) {
-      frames_since_transition = 0;
+        if (window_changed) {
+            seconds_since_transition = 0.0;
+        }
+
+        BeginDrawing();
+
+            texPosition.x += 2 * BACKGROUND_SPEED;
+            texPosition.y += 1 * BACKGROUND_SPEED;
+            texPosition = Vector2Mod(texPosition, (Vector2){texSource.width, texSource.height});
+            texPosition = Vector2Subtract(texPosition, texSize);
+            ClearBackground(BLACK);
+
+            if (window_mode == WINDOW_MODE_MAIN_MENU) {
+                if (seconds_since_transition<1) {
+                    // Get color lerp interpolation between two colors, factor [0.0f..1.0f]
+                    DrawRectangle(0, 0, windowSize.x, windowSize.y, Fade(RAYWHITE, seconds_since_transition));
+                } else {
+                    int isTransitioning = seconds_since_transition<2;
+                    float fade = 1.0;
+                    if (isTransitioning) {
+                        fade = seconds_since_transition-1;
+                    }
+                    DrawBackgroundTexture(tex, texSource, texPosition, fade*0.6);
+
+                    char *fitnessTitle = "Fitness";
+                    int LengthOfFitnessTitle = MeasureText(fitnessTitle, fontSize);
+                    Vector2 TextSize = {LengthOfFitnessTitle, fontSize};
+                    Vector2 TextPos = Vector2Subtract(screenMiddle, Vector2Scale( TextSize, 0.5));
+                    DrawTextV(fitnessTitle, TextPos, fontSize, Fade(titleColor,fade));
+
+                    if (!isTransitioning) {
+                        Vector2 recSize = Vector2Multiply(windowSize, (Vector2){0.11, 0.2});
+                        Vector2 recMiddle = {screenMiddle.x, windowSize.y*(11.0/15.0)};
+                        Vector2 recTopLeft = Vector2Subtract(recMiddle, Vector2Scale(recSize,0.5));
+                        Rectangle r = Vector2ToRectangle(recTopLeft, recSize);
+                        buttonOn = GuiButton(r, "Calender");
+                    }
+
+                    ClearBackground(RAYWHITE);
+                }
+                DrawFPS(10, 10);
+            }
+            if (buttonOn) {
+                printf("ON!!\n");
+            }else{
+                printf("OFF!!\n");
+            }
+
+            fflush(stdout);
+        EndDrawing();
     }
 
-    BeginDrawing();
-      texPosition.x = (float)((2 + (int)texPosition.x) % (int)texSource.width);
-      texPosition.y = (float)((1 + (int)texPosition.y) % (int)texSource.height);
-      ClearBackground(BLACK);
-
-      if (window_mode == WINDOW_MODE_MAIN_MENU) {
-        if (frames_since_transition<60) {
-          // Get color lerp interpolation between two colors, factor [0.0f..1.0f]
-          DrawRectangle(0, 0, screenWidth, screenHeight, Fade(RAYWHITE, (float)frames_since_transition/60.0f));
-        } else if (frames_since_transition<60*2) {
-          Color titleFade = Fade(titleColor, (float)(frames_since_transition-60)/60.0f);
-          Color whiteFade = Fade(WHITE, (float)(frames_since_transition-60)/60.0f);
-          DrawTextureRec(tex, texSource, (Vector2){texPosition.x                  , texPosition.y                   }, whiteFade);            // Draw a part of a texture defined by a rectangle
-          DrawTextureRec(tex, texSource, (Vector2){texPosition.x - texSource.width, texPosition.y                   }, whiteFade);            // Draw a part of a texture defined by a rectangle
-          DrawTextureRec(tex, texSource, (Vector2){texPosition.x                  , texPosition.y - texSource.height}, whiteFade);            // Draw a part of a texture defined by a rectangle
-          DrawTextureRec(tex, texSource, (Vector2){texPosition.x - texSource.width, texPosition.y - texSource.height}, whiteFade);            // Draw a part of a texture defined by a rectangle
-          DrawText(fitnessTitle, (screenWidth-LengthOfFitnessTitle)/2 , screenHeight/2 - 50, fontSize, titleFade);
-          ClearBackground(RAYWHITE);
-        } else {
-          DrawTextureRec(tex, texSource, (Vector2){texPosition.x                  , texPosition.y                   }, WHITE);            // Draw a part of a texture defined by a rectangle
-          DrawTextureRec(tex, texSource, (Vector2){texPosition.x - texSource.width, texPosition.y                   }, WHITE);            // Draw a part of a texture defined by a rectangle
-          DrawTextureRec(tex, texSource, (Vector2){texPosition.x                  , texPosition.y - texSource.height}, WHITE);            // Draw a part of a texture defined by a rectangle
-          DrawTextureRec(tex, texSource, (Vector2){texPosition.x - texSource.width, texPosition.y - texSource.height}, WHITE);            // Draw a part of a texture defined by a rectangle
-          DrawText(fitnessTitle, (screenWidth-LengthOfFitnessTitle)/2, screenHeight/2 - 50, fontSize, BLACK);
-        }
-        DrawFPS(10, 10);
-      }
-    EndDrawing();
-  }
-
-  UnloadTexture(tex);
-  CloseWindow();
+    UnloadTexture(tex);
+    CloseWindow();
 }
