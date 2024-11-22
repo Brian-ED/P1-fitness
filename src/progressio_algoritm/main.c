89d3250b19afc89794c8f8ac5a9786c4dbbfca3b
Gajoe1 <GM66JO@student.aau.dk>
2024-11-22 14:05:43 +0100

    y

--- src/progressio_algoritm/main.c
+++ src/progressio_algoritm/main.c
@@ -1,4 +1,17 @@
-char arr[] = "abcdef";
+#include <stdio.h>
+#include "progression.c"
 
-arr[];
-&arr[];
+void scan_prog(char *exercise);
+
+int main(void){
+
+
+    char exercise[80] = "bench press";
+
+    scan_prog(exercise);
+    printf("hello");
+
+
+
+    return 0;
+}
--- src/progressio_algoritm/progression.c
+++ src/progressio_algoritm/progression.c
@@ -1,2 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+
+
+void scan_prog(char *exercise){
+
+    char *name = strcat(exercise, ".prog.txt");
+    printf("%s", name);
+
+
+    FILE* ex_prog = fopen(name, "r");
+
+    if (ex_prog == NULL){
+        FILE* ex_prog = fopen(name, "ab+");
+    }
+
+     if (getc(ex_prog) == EOF){
+        printf("please enter the starting weight");
+    }
+    else 
+        printf("your ")
+
+
+    fclose(ex_prog);
+
+}
+
 
