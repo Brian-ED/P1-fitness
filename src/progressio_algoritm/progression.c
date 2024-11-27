6858ad7538baf7b5850d2d498a8011e9b7cb7896
JonasNU42 <juldah24@student.aau.dk>
2024-11-27 11:16:04 +0100

    h

--- src/progressio_algoritm/progression.c
+++ src/progressio_algoritm/progression.c
@@ -16,13 +16,22 @@ void scan_program(char *exercise){
     if(getc(ex_prog) == EOF){
         FILE* ex_prog = fopen(name, "w");
         printf("please enter the starting weight");
+        scanf("%d",text);
+        fprintf(ex_prog, " %d", text);
+        fclose(ex_prog);
+    } else {
+        FILE* ex_prog = fopen(name, "a");
+        printf("Please enter you new weight:");
+        scanf("%d",text);
+        fprintf(ex_prog, " %d", text);
+        fclose(ex_prog);
     }
-    else 
-        printf("your ")
+}
 
-
-    fclose(ex_prog);
+void calculate_new_weight(void){
 
 }
 
+void regression(){
 
+}
