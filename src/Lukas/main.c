fc86212e80eff51c4b15ea6492f97f462033027b
Gajoe1 <GM66JO@student.aau.dk>
2024-12-04 09:44:30 +0100

    temporary progression algorithm

--- src/Lukas/main.c
+++ src/Lukas/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 #include "progression.c"
 
 void scan_prog(char *exercise);
 
-int main(void){
 
+int main(void){
 
     char exercise[80] = "bench press";
 
     scan_prog(exercise);
 
-
-
     return 0;
 }
+
+
