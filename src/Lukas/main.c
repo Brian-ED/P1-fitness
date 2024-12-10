e9c134168d00f1ce5189b30f2216d793852f8a36
Brian-ED <brianellingsgaard9@gmail.com>
2024-12-10 13:20:34 +0100

    cleaned up same file

--- src/Lukas/main.c
+++ src/Lukas/main.c
@@ -2,15 +2,9 @@
 #include <math.h>
 #include "progression.c"
 
-void scan_prog(char *exercise);
-
-
 int main(void){
-
     char exercise[80] = "rows";
-
     scan_prog(exercise);
-
     return 0;
 }
 
