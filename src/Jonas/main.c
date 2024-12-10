bd3ec292e64879e5a4b46f5ed66da0671a52a7f6
Brian-ED <brianellingsgaard9@gmail.com>
2024-12-10 13:19:47 +0100

    cleaned up file

--- src/Jonas/main.c
+++ src/Jonas/main.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 #include "progression.c"
 
-void scan_prog(char *exercise);
-
 int main(void){
-
-
     char exercise[80] = "bench press";
-
     scan_prog(exercise);
-
-
 
     return 0;
 }
