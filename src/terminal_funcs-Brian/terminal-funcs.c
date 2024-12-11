5cbfc02fb8aba2b5caf155921ade3744a1ae5bc8
Brian-ED <brianellingsgaard9@gmail.com>
2024-12-11 13:11:26 +0100

    moved functions around a bit

--- src/terminal_funcs-Brian/terminal-funcs.c
+++ src/terminal_funcs-Brian/terminal-funcs.c
@@ -24,8 +24,79 @@ int DataFileExists() {
 
 void ChangeWorkoutViaAskingQuestions() {
 
-    // How many days a week
-    program(); // Also calculates calories needed and protein goals
+    if (DoesUserWantToChangeDaysPerWeek()) {
+    }
+        // How many days a week
+        program(); // Also calculates calories needed and protein goals
+        // LikeOrDislikeCertainExercises();
 
-    // TODO more questions
+    if (change_exercises_or_not()) {
+    }
+}
+
+void AskForWeight(void) {
+    printf("What is your current weight(kg)? :");
+    scanf("%d",&weight);
+}
+
+int DoesUserWantToChangeWorkout(void) {
+    char m[3] = "";
+    printf("Do you want to change the workout? Please type 'yes' or 'no'");
+    scanf("%s", m);
+
+    if (!strcmp(m, "yes")) {
+        return 1;
+    }
+    if (!strcmp(m, "no")) {
+        return 0;
+    }
+
+    printf("Not valid answer");
+    exit(1);
+}
+
+int DoesUserWantToChangeOptions(void) {
+    char m[3] = "";
+    printf("Do you want to change any options? Please type 'yes' or 'no'");
+    scanf("%s", m);
+
+    if (!strcmp(m, "yes")) {
+        return 1;
+    }
+    if (!strcmp(m, "no")) {
+        return 0;
+    }
+
+    printf("Not valid answer");
+    exit(1);
+}
+
+int DoesUserWantToChangeDaysPerWeek() {
+    char m[3] = "";
+    printf("Do you want to change the amount of workout days per week? Please type 'yes' or 'no'");
+    scanf("%s", m);
+
+    if (!strcmp(m, "yes")) {
+        return 1;
+    }
+    if (!strcmp(m, "no")) {
+        return 0;
+    }
+    printf("Not valid answer");
+    exit(1);
+}
+
+int DoesUserWantToStartAWorkoutSession(void) {
+    char m[3] = "";
+    printf("Do you want to start a workout? Please type 'yes' or 'no'");
+    scanf("%s", m);
+
+    if (!strcmp(m, "yes")) {
+        return 1;
+    }
+    if (!strcmp(m, "no")) {
+        return 0;
+    }
+    printf("Not valid answer");
+    exit(1);
 }
--- src/welcome_back/main.c
+++ src/welcome_back/main.c
@@ -4,6 +4,5 @@ int main(){
     show_welcome_back();
     change_program_or_exercises();
     update_personal_info_or_not();
-    change_days_a_week_or_not();
     change_exercises_or_not();
 }
