5cca4a24e51b2dd7d4d235df260fa7a62420ffde
Brian-ED <brianellingsgaard9@gmail.com>
2024-12-12 22:45:47 +0100

    added debug statements

--- main.c
+++ main.c
@@ -20,27 +20,32 @@ void SaveRepsToWorkoutFile() {} // Lukas
 // #include "src/frontend-brian/frontend.c"
 
 void FLOW_EndApp() {
+    if (DEBUG) {printf("EndApp");}
     EndApp();
     exit(0);
 }
 
 void FLOW_SaveRepsToWorkoutFile() {
+    if (DEBUG) {printf("SaveRepsToWorkoutFile");}
     SaveRepsToWorkoutFile();
     FLOW_EndApp();
 }
 
 void FLOW_DoEachSet() {
+    if (DEBUG) {printf("DoEachSet");}
     DoEachSet();
     FLOW_SaveRepsToWorkoutFile();
 }
 
 void FLOW_ReadInDataFileAndGetDate() {
+    if (DEBUG) {printf("ReadInDataFileAndGetDate");}
     ReadInDataFile();
     GetDate();
     FLOW_DoEachSet();
 }
 
 void FLOW_DoesUserWantToStartAWorkoutSession() {
+    if (DEBUG) {printf("DoesUserWantToStartAWorkoutSession");}
     if (DoesUserWantToStartAWorkoutSession()) {
         FLOW_ReadInDataFileAndGetDate();
     } else {
@@ -49,17 +54,20 @@ void FLOW_DoesUserWantToStartAWorkoutSession() {
 }
 
 void FLOW_ShowProgressionViaDataIfEnoughWorkoutsSavedInData() {
+    if (DEBUG) {printf("ShowProgressionViaDataIfEnoughWorkoutsSavedInData");}
     ShowProgressionViaDataIfEnoughWorkoutsSavedInData();
     FLOW_DoesUserWantToStartAWorkoutSession();
 }
 
 void FLOW_AskForWeightAndCalculateCaloryIntake() {
+    if (DEBUG) {printf("AskForWeightAndCalculateCaloryIntake");}
     AskForWeight();
     CalculateCaloryIntake();
     FLOW_ShowProgressionViaDataIfEnoughWorkoutsSavedInData();
 }
 
 void FLOW_DoesUserWantToAddNewWeight() {
+    if (DEBUG) {printf("DoesUserWantToAddNewWeight");}
     if (DoesUserWantToAddNewWeight()) {
         FLOW_AskForWeightAndCalculateCaloryIntake();
     } else {
@@ -68,16 +76,19 @@ void FLOW_DoesUserWantToAddNewWeight() {
 }
 
 void FLOW_SaveUserOptionsToFile() {
+    if (DEBUG) {printf("SaveUserOptionsToFile");}
     SaveUserOptionsToFile();
     FLOW_DoesUserWantToAddNewWeight();
 }
 
 void FLOW_ShowAndAskOptions() {
+    if (DEBUG) {printf("ShowAndAskOptions");}
     ShowAndAskAndSaveUserOptions();
     FLOW_SaveUserOptionsToFile();
 }
 
 void FLOW_DoesUserWantToChangeOptions() {
+    if (DEBUG) {printf("DoesUserWantToChangeOptions");}
     if (DoesUserWantToChangeOptions()) {
         FLOW_ShowAndAskOptions();
     } else {
@@ -86,16 +97,19 @@ void FLOW_DoesUserWantToChangeOptions() {
 }
 
 void FLOW_WriteChangedWorkoutToFile() {
+    if (DEBUG) {printf("WriteChangedWorkoutToFile");}
     WriteChangedWorkoutToFile();
     FLOW_DoesUserWantToChangeOptions();
 }
 
 void FLOW_ChangeWorkoutViaAskingUserQuestions() {
+    if (DEBUG) {printf("ChangeWorkoutViaAskingUserQuestions");}
     ChangeWorkoutViaAskingQuestions();
     FLOW_WriteChangedWorkoutToFile();
 }
 
 void FLOW_DoesUserWantToChangeWorkout() {
+    if (DEBUG) {printf("DoesUserWantToChangeWorkout");}
     if (DoesUserWantToChangeWorkout()) {
         FLOW_ChangeWorkoutViaAskingUserQuestions();
     } else {
@@ -104,21 +118,25 @@ void FLOW_DoesUserWantToChangeWorkout() {
 }
 
 void FLOW_SaveProgramToWorkoutFile() {
+    if (DEBUG) {printf("SaveProgramToWorkoutFile");}
     SaveProgramToWorkoutFile();
     FLOW_DoesUserWantToChangeWorkout();
 }
 
 void FLOW_CreateDefaultProgram() {
+    if (DEBUG) {printf("CreateDefaultProgram");}
     CreateDefaultProgram();
     FLOW_SaveProgramToWorkoutFile();
 }
 
 void FLOW_DisplayIntroduction() {
+    if (DEBUG) {printf("DisplayIntroduction");}
     DisplayIntroductionMessage();
     FLOW_CreateDefaultProgram();
 }
 
 void FLOW_AskUserIfTheyWantToChangeOptionsAndOrWorkout() {
+    if (DEBUG) {printf("AskUserIfTheyWantToChangeOptionsAndOrWorkout");}
     // User has been in the application before, so we say welcome back.
     show_welcome_back();
 
@@ -129,7 +147,8 @@ void FLOW_AskUserIfTheyWantToChangeOptionsAndOrWorkout() {
     }
 }
 
-void FLOW_DoesDataFileExist(){
+void FLOW_DoesDataFileExist() {
+    if (DEBUG) {printf("DoesDataFileExist");}
     if (DoesDataFileExist()) {
         FLOW_AskUserIfTheyWantToChangeOptionsAndOrWorkout();
     } else {
@@ -138,6 +157,7 @@ void FLOW_DoesDataFileExist(){
 }
 
 void FLOW_start_app() {
+    if (DEBUG) {printf("start_app");}
     FLOW_DoesDataFileExist();
 }
 
