e7886449d191fae6b86831c381b073dc13fc260c
Brian-ED <brianellingsgaard9@gmail.com>
2024-12-13 09:18:05 +0100

    fixed bugs and moved function

--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define BUF_SIZE 65536
 
 FILE *openSafe(char *filename, char *mode) {
     if (filename == NULL) {
@@ -17,7 +18,7 @@ FILE *openSafe(char *filename, char *mode) {
     return file;
 }
 
-File *openCreate(char *filename, char *mode) {
+FILE *openCreate(char *filename, char *mode) {
     if (filename == NULL) {
         printf("ERROR: openSafe: filename was NULL pointer\n");
         exit(EXIT_FAILURE);
@@ -27,11 +28,11 @@ File *openCreate(char *filename, char *mode) {
         exit(EXIT_FAILURE);
     }
     FILE *file = fopen(filename, mode);
-    if (file =! NULL) return file;
+    if (file != NULL) return file;
 
     fclose(fopen(filename, "ab+"));
 
-    FILE *file = fopen(filename, mode);
+    file = fopen(filename, mode);
     if (file == NULL) {
         printf("ERROR: openSafe: Cannot find file %s\n", filename);
         exit(EXIT_FAILURE);
@@ -48,4 +49,24 @@ void copyFile(char *from_filepath, char *to_filepath) {
     }
     fclose(from);
     fclose(to);
+}
+
+// count_lines implementation found at https://stackoverflow.com/a/70708991
+int count_lines(FILE* file)
+{
+    char buf[BUF_SIZE];
+    int counter = 0;
+
+    while (!feof(file)) {
+        size_t res = fread(buf, 1, BUF_SIZE, file);
+        if (ferror(file))
+            return -1;
+
+        for(int i = 0; i < res; i++)
+            if (buf[i] == '\n')
+                counter++;
+
+    }
+    rewind(file);
+    return counter;
 }
